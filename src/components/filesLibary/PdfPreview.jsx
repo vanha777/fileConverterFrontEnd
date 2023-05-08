a4ed4e3ed7243ac112deed731fdad1c28840ff5c
vanha777 <vanha777@icloud.com>
2023-05-09 02:32:17 +1000

    9/5,2

--- src/components/filesLibary/PdfPreview.jsx
+++ src/components/filesLibary/PdfPreview.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5.js';
-import pdfFile from './test.pdf'
+
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
